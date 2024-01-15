551f5f4cbdffbe3d258c0cca4bff7fbc8776de09
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-01-15 22:45:20 +0900

    Domain framework 추가

---
+++ RxExercise/Domain/Source/Domain/Entities/BusModel.swift
@@ -0,0 +1,12 @@
+//
+//  BusModel.swift
+//  Domain
+//
+//  Created by Jisoo HAM on 1/15/24.
+//
+
+import Foundation
+
+public struct BusModel {
+    
+}
---
+++ RxExercise/Pods/Target Support Files/Pods-RxExercise/Pods-RxExercise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxExercise : NSObject
+@end
+@implementation PodsDummy_Pods_RxExercise
+@end
