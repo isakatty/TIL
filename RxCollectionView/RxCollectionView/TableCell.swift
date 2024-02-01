7dfb15d0ed102bbbd0ad9047fa536e408b2eca32
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-02-01 12:33:07 +0900

    uitableview와 uiview를 품는 uiscrollview 구성

---
+++ RxCollectionView/RxCollectionView/TableCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableCell.swift
+//  RxCollectionView
+//
+//  Created by Jisoo HAM on 1/30/24.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
