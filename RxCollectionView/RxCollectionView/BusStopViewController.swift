49072408dfb1cb23fe1442fead8652a5222ce8a4
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-02-01 12:34:45 +0900

    [Feat] headerView와 uitableview를 이용하여 uiscrollview 구성

--- RxCollectionView/RxCollectionView/BusStopViewController.swift
+++ RxCollectionView/RxCollectionView/BusStopViewController.swift
@@ -7,23 +7,122 @@
 
 import UIKit
 
-class BusStopViewController: UIViewController {
+import RxSwift
+import RxCocoa
 
+import RxDataSources
+
+class BusStopViewController: UIViewController {
+    
+    let viewModel = ViewModel()
+    let disposeBag = DisposeBag()
+    
+    private let headerView: BusStopInfoHeaderView = BusStopInfoHeaderView()
+    private var scrollView: UIScrollView = UIScrollView()
+    private let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .insetGrouped)
+        tv.register(TableCell.self,
+                    forCellReuseIdentifier: TableCell.identifier)
+        
+        tv.register(HeaderView.self,
+                    forHeaderFooterViewReuseIdentifier: HeaderView.identifier)
+        tv.isScrollEnabled = false
+        return tv
+    }()
+    private let contentView = UIView()
+    
+    var tableViewHeightConstraint = NSLayoutConstraint()
+    let dataSource = RxTableViewSectionedReloadDataSource<DataSection> { dataSource, tableView, indexPath, item in
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableCell.identifier, for: indexPath) as? TableCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        cell.bind(with: item)
+        
+        return cell
+    } 
+//titleForHeaderInSection: { dataSource, indexPath in
+//        return dataSource.sectionModels[indexPath].header
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        
+        configureUI()
+        
+        tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
+        
+        bind()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func bind() {
+        Observable.just(viewModel.sections)
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
-    */
+    
+    func configureUI() {
+        view.addSubview(scrollView)
+        
+        [scrollView, contentView, headerView, tableView]
+            .forEach { components in
+                components.translatesAutoresizingMaskIntoConstraints = false
+            }
+        
+        
+        [headerView, tableView]
+            .forEach { components in
+                contentView.addSubview(components)
+            }
+        
+        tableViewHeightConstraint = tableView.heightAnchor.constraint(equalToConstant: 0)
+        
+        scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            headerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            tableViewHeightConstraint,
+        ])
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableViewHeightConstraint.constant = tableView.contentSize.height
+    }
+}
 
+extension BusStopViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 55
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: HeaderView.identifier) as? HeaderView
+        else { return UIView() }
+        
+        headerView.configureHeader(with: dataSource.sectionModels[section].header)
+        
+        return headerView
+    }
+    
 }
--- RxCollectionView/RxCollectionView/ScrollViewController.swift
+++ RxCollectionView/RxCollectionView/ScrollViewController.swift
@@ -7,23 +7,82 @@
 
 import UIKit
 
-class ScrollViewController: UIViewController {
+import RxSwift
+import RxCocoa
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+import RxDataSources
 
-        // Do any additional setup after loading the view.
-    }
-    
+//class ScrollViewController: UIViewController {
+//    
+//    let scrollView : UIScrollView = UIScrollView()
+//    let studying: BusStopInfoHeaderView = BusStopInfoHeaderView()
+//    var collectionView = BusStopCollectionView()
+//    let viewModel = ViewModel()
+//    let disposeBag = DisposeBag()
+//    
+//    let BusStopView = UIView()
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        view.backgroundColor = .systemBackground
+//        
+//        configureUI()
+//        bind()
+//    }
+//    
+//    func bind() {
+//        Observable.just(viewModel.sections)
+//            .bind(to: collectionView.collectionView.rx.items(dataSource: collectionView.dataSource))
+//            .disposed(by: disposeBag)
+//    }
+//
+//}
+//
+//
+//extension ScrollViewController {
+//    func configureUI() {
+//        view.addSubview(scrollView)
+//        scrollView.addSubview(BusStopView)
+//        
+//        [scrollView, studying, collectionView, BusStopView]
+//            .forEach { components in
+//                components.translatesAutoresizingMaskIntoConstraints = false
+//            }
+//    
+//        [studying, collectionView]
+//            .forEach { components in
+//                BusStopView.addSubview(components)
+//            }
+//        
+//        collectionView.collectionView.isScrollEnabled = false
+//        
+//        NSLayoutConstraint.activate([
+//            // scrollview의 content guide 설정이 안됨!
+//            // lead, trail을 content guide에 맞춰주어야 scrollview 잡힘.
+//            scrollView.contentLayoutGuide.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            scrollView.contentLayoutGuide.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+//            //61, 62는 좌우 스크롤을 막기 위함. contentLayoutGuide
+//            
+//            scrollView.frameLayoutGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            scrollView.frameLayoutGuide.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//            
+//            BusStopView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+//            BusStopView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+//            BusStopView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+//            BusStopView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+//            studying.topAnchor.constraint(equalTo: BusStopView.topAnchor),
+//            studying.leadingAnchor.constraint(equalTo: BusStopView.leadingAnchor),
+//            studying.trailingAnchor.constraint(equalTo: BusStopView.trailingAnchor),
+//            collectionView.topAnchor.constraint(equalTo: studying.bottomAnchor),
+//            collectionView.leadingAnchor.constraint(equalTo: BusStopView.leadingAnchor),
+//            collectionView.trailingAnchor.constraint(equalTo: BusStopView.trailingAnchor),
+//            collectionView.bottomAnchor.constraint(equalTo: BusStopView.bottomAnchor)
+//            
+//            
+//        ])
+//    }
+//}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
